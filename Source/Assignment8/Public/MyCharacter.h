4b134b99cf80e1dd311fa8884cf35cadd81a405c
JongKyu <kongi7064@gmail.com>
2026-01-23 20:25:05 +0900

    default settings

---
+++ Source/Assignment8/Public/MyCharacter.h
@@ -0,0 +1,55 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "MyCharacter.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
+struct FInputActionValue;
+
+UCLASS()
+class ASSIGNMENT8_API AMyCharacter : public ACharacter
+{
+	GENERATED_BODY()
+public:
+	AMyCharacter();
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Camera")
+	USpringArmComponent* SpringArmComp;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Camera")
+	UCameraComponent* CameraComp;
+
+#pragma region Action
+	UFUNCTION()
+	void Move(const FInputActionValue& value);
+	UFUNCTION()
+	void StartJump(const FInputActionValue& value);
+	UFUNCTION()
+	void StopJump(const FInputActionValue& value);
+	UFUNCTION()
+	void Look(const FInputActionValue& value);
+	UFUNCTION()
+	void StartSprint(const FInputActionValue& value);
+	UFUNCTION()
+	void StopSprint(const FInputActionValue& value);
+#pragma endregion
+
+#pragma region Movement
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	float NormalSpeed;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Movement")
+	float SprintSpeedMultiplier;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Movement")
+	float SprintSpeed;
+#pragma endregion
+
+	
+	
+public:	
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+};
---
+++ Source/Assignment8/Public/MyGameMode.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "MyGameMode.generated.h"
+
+UCLASS()
+class ASSIGNMENT8_API AMyGameMode : public AGameMode
+{
+	GENERATED_BODY()
+public:
+	AMyGameMode();
+};
